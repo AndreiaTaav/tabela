0948a1c09ee665290aa357b4306ebebc084b2eaf
AndreiaTaav <andreia.tavares@outlook.com>
2014-02-04 16:39:05 -0200

    Create tabela.php

---
+++ tabela.php
@@ -0,0 +1,32 @@
+<table border>
+    <thead>
+        <tr>
+            <th>Danca Moderna</th>
+            <th>Danca Contempor&acirc;nea</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Hor&aacute;rios</td>
+            <td>Hor&aacute;rios</td>
+        </tr>
+        
+        <tr>
+            <td>08:30 as 11:00</td>
+            <td>13:00 as 16:00</td>
+        </tr>
+    </tbody>
+    
+    <tbody>
+        <tr>
+            <td>Dias da Semana</td>
+            <td>Dias da Semana</td>
+        </tr>
+        
+        <tr>
+            <td>Seg, Qua , Sex</td>
+            <td>Ter, Qui , Sab</td>
+        </tr>
+    </tbody>
+    
+</table border>
